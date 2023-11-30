c48e74822e55ceb8271164523c0658d02efe457f
Luc Gouzy <lg-1@live.fr>
2023-11-30 01:12:37 +0100

    fixed router-link showing twice + style
    added transition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>
-    <router-link to="/second-page">Second Page</router-link>
-
     <router-view></router-view>
   </div>
 </template>
